3d44dd0555c875b899d251add3fbd9fd39b70d9f
Matthew Leon <matthew.leon@vanderbilt.edu>
2019-02-12 11:21:23 -0600

    Rework examples for 1st session/delete extra files

--- 00-example-project/src/intellijTipsAndTricks.java
+++ 00-example-project/src/intellijTipsAndTricks.java
@@ -1,40 +1,61 @@
+/*
+ * TODO: Add your name!
+ * 2/12/2019
+ *
+ * Description: Debugging examples used to practice Java debugging in CS 1101.
+ */
+
 import java.util.Scanner;
 
 public class intellijTipsAndTricks {
     public static void main(String[] args) {
-        // Prompt for user input
-        Scanner console = new Scanner(System.in);
+// Prompt for user input
+         Scanner console = new Scanner(System.in);
         System.out.println("What word would you like to use to debug?");
-        String str = console.nextLine();
 
+            String str = console.nextLine();
         debuggingExample(str);
     }
 
-    /*
-     * badStyleExample - this does x y and z
-     */
-    private static void badStyleExample() {
-        System.out.println();
+// TODO: Fix Javadoc comment block
+/*
+* isSame - this compares a and b
+*/
+public boolean isSame(boolean a, boolean b) {
+if (a && b) {
+return true;
+} else if (!a && !b) {return true;} else {
+return false;
+        // You could write this in one line! Try it!
+}}
 
-        for (int i = 0; i < 4; i++) {
-            debuggingExample("hello");
-        }
-    }
 
     /**
      * badstyleexampl2 - an example of BAD style
      */
-    private static void badstyleexampl2() {
-        System.out.println();
+    public static void badstyleexampl2() {
+        char myChar = '\0';              // this is a valid char
+        String word = "hello";
+
         for (int i = 0; i < 4; i++) {
-            debuggingExample("hello");
+            myChar = word.charAt(i);
         }
+
+        System.out.println(myChar);
+        // What the heck does this print out???
+        // Why couldn't you have just written `char printLetter = 'l';`?
+        // Is there a time when myChar _isn't_ a valid char?
     }
 
     /**
-     * debuggingExample - helps demonstrate basic functionality of a debugger
+     * debuggingExample - helps demonstrate basic functionality of a debugger.
+     * Calls debuggingHelper() on every letter.
+     *
+     * This method is a good example of javadoc comments.
+     *
+     * @param str String to debug
      */
-    private static void debuggingExample(String str) {
+    public static void debuggingExample(String str) {
         char[] arr = str.toCharArray(); // Converts str to a char[]
 
         // Iterate through arr and do something fun with every char
@@ -45,8 +66,10 @@ public class intellijTipsAndTricks {
 
     /**
      * debuggingHelper - helper function for debuggingExample()
+     * @param c char to print
      */
-    private static void debuggingHelper(char c) {
+    public static void debuggingHelper(char c) {
+        // Rick and morty joke
         System.out.println("Ooh wee, Rick, I'm helping with the letter " + c + "!");
     }
 }
--- README.md
+++ README.md
@@ -5,16 +5,17 @@ This repository contains notes and slide decks for bonus lectures I teach for CS
 Here's a high-level breakdown of the directory structure:
 + ```00-example-project/``` - A sample IntelliJ project associated with my lectures.
 + ```01-intellij-tips-and-tricks/``` - Slides and notes for lecture 01.
-+ ```02-dealing-with-debugging/``` - Slides and notes for lecture 02.
-+ ```03-moving-forward-in-cs/``` - Slides and notes for lecture 03.
 
 ## Who are you?
-My name is Irfaan Khalid. I'm a passionate software engineer, hackathon organizer, and aspiring teacher. I'm a TA for CS 1101 - an introductory Java course - at Vanderbilt University, and absolutely love guiding and mentoring students as they enter the field of computer science.
+My name is Matt Leon. I'm a passionate software engineer, hackathon organizer, and web developer. I'm a TA for CS 1101 - an introductory Java course - at Vanderbilt University, and absolutely love guiding and mentoring students as they enter the field of computer science.
 
 ## Why does this exist?
 Like I said, there's a slew of topics for which there simply isn't enough time during lecture to cover. I hope for it to help students by serving as a supplement to their lectures and providing some perspective into _why_ CS 1101 matters and what software development looks like after the course.
 
 ## Where can I submit questions or feedback?
-If you have questions or feedback, feel free to email me at [irfaan.khalid@vanderbilt.edu](mailto:irfaan.khalid@vanderbilt.edu).
+If you have questions or feedback, feel free to email me at [matthew.leon@vanderbilt.edu](mailto:matthew.leon@vanderbilt.edu).
 
 If you find an error anywhere in the guide, please feel free to make a pull request on GitHub.
+
+## Credits
+This repo and the accompanying slides were originally created by [Irfaan Khalid](https://irfaan.me) and updated with new examples and pictures by me.
